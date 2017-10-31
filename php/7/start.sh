cd64966147674b052998143f6b9ea20c0c9250f3
Alistair Scott <ali.scott@gmail.com>
2017-10-31 16:41:19 +0000

    Update PHP 7 docs

--- php/7/start.sh
+++ php/7/start.sh
@@ -1,9 +1,16 @@
+# You can use the `ENTRYPOINT` DeploymentConfig environment variable to specify
+# which command to run. This allows you to use the same Dockerfile for the
+# web and worker processes. You can customize this script to add any other
+# entrypoints you would like. This script contains the basics for a standard
+# Laravel app.
 #!/bin/bash
 
+# If the entrypoint is `workers` we run the Laravel worker
 if [ "$ENTRYPOINT" = "workers" ]; then
   echo Starting workers
   php artisan queue:work --tries=3
 
+# If the entrypoint is `schedule_run` we run the Laravel scheduler
 elif [ "$ENTRYPOINT" = "schedule_run" ]; then
   echo Starting schedule_run
   while [ 1 ]
@@ -12,6 +19,7 @@ elif [ "$ENTRYPOINT" = "schedule_run" ]; then
     sleep 60
   done
 
+# If the entrypoint is blank or `web` we want to run the web supervisord process
 elif [ -z "$ENTRYPOINT" ] || "$ENTRYPOINT" = "web" ]
 then
   echo Starting web
