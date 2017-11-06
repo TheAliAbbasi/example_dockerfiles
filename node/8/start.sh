d3d0f85d1b79b19368edecafe042d4f9de76cd4f
Alistair Scott <ali.scott@gmail.com>
2017-11-06 18:18:33 +0000

    Add node docs

--- node/8/start.sh
+++ node/8/start.sh
@@ -1,9 +1,15 @@
+# Use the `ENTRYPOINT` DeploymentConfig environment variable to specify
+# which command to run. This enables the same Dockerfile to be used for
+# web and worker processes.
 #!/bin/bash
 
+# If the entrypoint is `workers`, run the node worker.
 if [ "$ENTRYPOINT" = "workers" ]
 then
   echo Starting workers
   # Put any commands here to start background workers
+
+# If the entrypoint is blank or `web`, run the web supervisord process.
 elif [ -z "$ENTRYPOINT" ] || "$ENTRYPOINT" = "web" ]
 then
   echo Starting web
